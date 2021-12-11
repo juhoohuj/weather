e46184082b6849129592e8f69147763abd052953
Juho Ahonen <93704940+juhoohuj@users.noreply.github.com>
2021-12-11 19:30:31 +0200

    added files

---
+++ weather.js
@@ -0,0 +1,32 @@
+
+let button = document.getElementById("search")
+
+button.addEventListener("click", weatherToday)
+
+function weatherToday() {
+
+    let city = document.getElementById("haku").value
+    let searchedCity = document.getElementById("city")
+    let xhttp = new XMLHttpRequest();
+    let errorMsg = document.getElementById("error")
+
+    xhttp.onreadystatechange = function() {
+        errorMsg.innerHTML = "";
+        let weather = JSON.parse(this.response)
+	    if (this.readyState == 4 && this.status == 200) {
+	     // Access the result here
+         document.getElementById("city").innerHTML = weather.name + "<br>"
+	     document.getElementById("weather").innerHTML =  "Lämpötila nyt: " + Math.round(weather.main.temp) + "<br>" + "Tuntuu kuin: " + Math.round(weather.main.feels_like);
+        }
+        
+        if (searchedCity.innerHTML === "") {
+        errorMsg.innerHTML = "Kaupunkia ei löytynyt";
+        return;
+    }
+    };
+    xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=+" + city + "&units=metric&appid=377080a829c7f0df00f8dfbd81bca2ff", true);
+    xhttp.send();
+
+    }
+
+
