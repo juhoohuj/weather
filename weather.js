2a860911db5a68698571f148e36c6d669b8fc6bd
Juho Ahonen <juho.ahonen2@gmail.com>
2022-01-07 18:09:07 +0200

    responsiivisuus, lisätty "äskettäin haetut" toiminnallisuus, muokattu tyylejä

--- weather.js
+++ weather.js
@@ -1,10 +1,12 @@
-
+let counter = 0
 let searchButton = document.getElementById("search")
-
 let firstCountry = document.getElementById("first")
 let secondCountry = document.getElementById("second")
 let thirdCountry = document.getElementById("third")
-
+firstCountry.addEventListener("click", searchedCountry)
+secondCountry.addEventListener("click", searchedCountry2)
+thirdCountry.addEventListener("click", searchedCountry3)
+let searchedCountries = [firstCountry, secondCountry, thirdCountry]
 searchButton.addEventListener("click", weatherToday)
 
 function capitalizeFirstLetter(string) {
@@ -18,7 +20,6 @@ function weatherToday() {
 
     xhttp.onreadystatechange = function() {
 
-
         errorMsg.innerHTML = "";
         let weather = JSON.parse(this.response)
 	    if (this.readyState == 4 && this.status == 200) {
@@ -29,14 +30,15 @@ function weatherToday() {
             document.getElementById("feelslike").innerHTML = "Tuntuu kuin: " + Math.round(weather.main.feels_like) + "&deg;";
             document.getElementById("lowest").innerHTML = "Alin lämpötila: " + Math.round(weather.main.temp_min) + "&deg;";
             document.getElementById("highest").innerHTML = "Ylin lämpötila: " + Math.round(weather.main.temp_max) + "&deg;";
-
             document.getElementById("desc").innerHTML = "Kuvaus: " + weather.weather[0].description;
-
             document.getElementById("wind").innerHTML = "Tuuli: " + weather.wind.speed + " m/s";
             document.getElementById("cloudiness").innerHTML = "Pilvisyys: " + weather.clouds.all + "%";
-            
 
-            
+            if (counter == 3) {
+                counter = 0
+            }
+            searchedCountries[counter].innerHTML = city;
+            counter++
         }
         
         else {
@@ -50,5 +52,17 @@ function weatherToday() {
 
 
     
+function searchedCountry() {
+    document.getElementById("haku").value = firstCountry.innerText
+    weatherToday();
+}
 
+function searchedCountry2() {
+    document.getElementById("haku").value = secondCountry.innerText
+    weatherToday();
+}
 
+function searchedCountry3() {
+    document.getElementById("haku").value = thirdCountry.innerText
+    weatherToday();
+}
